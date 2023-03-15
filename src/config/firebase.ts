fb7046fced50ae2bba7a1e9e4def54fbd2e53f1b
karzQ <karczinski.quentin@gmail.com>
2023-03-15 22:48:05 +0100

    added firebase to project

---
+++ src/config/firebase.ts
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+
+import { initializeApp } from "firebase/app";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCiibitf7e-aOyHofdX8bDYA7pBVUZo3gw",
+  authDomain: "emos-website.firebaseapp.com",
+  projectId: "emos-website",
+  storageBucket: "emos-website.appspot.com",
+  messagingSenderId: "199542208287",
+  appId: "1:199542208287:web:a402d17447628cdb55222a"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
